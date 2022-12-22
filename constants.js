f452ba63c87e055908a10b7f6903c13c26dccca0
李槟 <1961774471@qq.com>
2022-12-22 11:10:20 +0800

    同步组件

--- constants.js
+++ constants.js
@@ -1,2 +1,2 @@
 
-export const DEVELOPER_ENV = process.env.NODE_ENV === 'development' ? 'true' : 'false';
+export const DEVELOPER_ENV = "true" // process.env.NODE_ENV === 'development' ? 'true' : 'false';
