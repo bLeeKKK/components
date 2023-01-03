7d54a1fa1409a1137a9ccc636dab5e4af0508091
李槟 <1961774471@qq.com>
2023-01-03 15:42:15 +0800

    添加icon配置

--- icons.js
+++ icons.js
@@ -3,3 +3,7 @@ export const ICON_EDIT = 'edit'; // 编辑
 export const ICON_DELETE = 'delete'; // 刪除
 export const ICON_PLUS = 'plus'; // 加号
 export const ICON_PAPER_CLIP = 'paper-clip'; // 附件
+
+export const ICON_FROZEN = 'pause-circle'; // 冻结
+export const ICON_FROZEN_THAW = 'play-circle'; // 解冻
+
