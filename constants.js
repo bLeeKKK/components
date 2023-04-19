a32eeea0ac52082241397508b60de06acb8fe551
李槟 <1961774471@qq.com>
2023-04-19 14:09:34 +0800

    同步常量

--- constants.js
+++ constants.js
@@ -3,8 +3,8 @@
 // 权限控制
 export const DEVELOPER_ENV = process.env.NODE_ENV === 'development' ? 'true' : 'false';
 
+const domain = window.top.location.href;
 //  责任部门根节点id
-export const ACCOUNTABILITY_ID =
-  process.env.NODE_ENV === 'development'
-    ? '37702411-81A0-11ED-82E5-0242AC14001C'
-    : '9D1E3159-B2F3-11EA-A328-0242C0A84408';
+export const ACCOUNTABILITY_ID = domain.includes('changhong')
+  ? '9D1E3159-B2F3-11EA-A328-0242C0A84408'
+  : '37702411-81A0-11ED-82E5-0242AC14001C';
