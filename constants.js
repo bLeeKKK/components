3949469754cf7a8dc6f1c97c0133096ff1039076
李槟 <1961774471@qq.com>
2023-04-19 09:08:55 +0800

    同步常量

--- constants.js
+++ constants.js
@@ -1,3 +1,10 @@
+// export const DEVELOPER_ENV = 'true'; // process.env.NODE_ENV === 'development' ? 'true' : 'false';
 
-export const DEVELOPER_ENV = "true" // process.env.NODE_ENV === 'development' ? 'true' : 'false';
-// export const DEVELOPER_ENV = process.env.NODE_ENV === 'development' ? 'true' : 'false';
+// 权限控制
+export const DEVELOPER_ENV = process.env.NODE_ENV === 'development' ? 'true' : 'false';
+
+//  责任部门根节点id
+export const ACCOUNTABILITY_ID =
+  process.env.NODE_ENV === 'development'
+    ? '37702411-81A0-11ED-82E5-0242AC14001C'
+    : '9D1E3159-B2F3-11EA-A328-0242C0A84408';
